2eaf7615da626df95d18d9e5676ee5ce74d19112
Brenden Bissett <brenden.bissett@gmail.com>
2022-04-28 22:34:26 +1200

    Treesitter 'ensure_installed = "maintained"' is being depricated on 30 April 2022. Changed value to "all"

--- dot_config/nvim/lua/nvim-treesitter-config/init.lua
+++ dot_config/nvim/lua/nvim-treesitter-config/init.lua
@@ -1,6 +1,6 @@
 local configs = require('nvim-treesitter.configs')
 configs.setup{
-	ensure_installed = "maintained", -- Only use parsers that are maintained
+	ensure_installed = "all",
 	highlight = {
 		enable = true,
 	},
